e3318d6c9c623a466ad5665e9c37ed5056cbd4e0
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2025-04-08 16:41:44 -0700

    wip: style the dashboard with a card component

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,12 +1,11 @@
 import { useEffect, useState, useContext } from 'react';
-
 import { UserContext } from '../../contexts/UserContext';
-
 import * as userService from '../../services/userService';
+import './Dashboard.css';
 
 const Dashboard = () => {
   const { user } = useContext(UserContext);
-  const [ users, setUsers ] = useState([]);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -14,23 +13,24 @@ const Dashboard = () => {
         const fetchedUsers = await userService.index();
         setUsers(fetchedUsers);
       } catch (err) {
-        console.log(err)
+        console.log(err);
       }
-    }
+    };
     if (user) fetchUsers();
   }, [user]);
 
   return (
-    <main>
-      <h1>Welcome, {user.username}</h1>
-      <p>
-        This is the dashboard page where you can see a list of all the users.
-      </p>
-      <ul>
-        {users.map(user => (
-          <li key={user._id}>{user.username}</li>
-        ))}
-      </ul>
+    <main className="dashboard">
+      <div className="card">
+        <h1>Welcome, {user.username}</h1>
+        <p>This is the dashboard page where you can see a list of all the users.</p>
+        <h2>All Users</h2>
+        <ul>
+          {users.map((user) => (
+            <li key={user._id}>{user.username}</li>
+          ))}
+        </ul>
+      </div>
     </main>
   );
 };
