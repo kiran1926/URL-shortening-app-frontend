a3548f12243f2977873337937abf0fa932ce1dd6
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2025-04-08 14:55:47 -0700

    wip: style landing page

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,8 +1,16 @@
+import './Landing.css';
+
 const Landing = () => {
   return (
-    <main>
-      <h1>Hello, you are on the landing page for visitors.</h1>
-      <p>Sign up now, or sign in to see your super secret dashboard!</p>
+    <main className="landing">
+      <div className="card">
+        <h1>Welcome to the Landing Page!</h1>
+        <p>Sign up now, or sign in to see your super secret dashboard!</p>
+        <div className="card-actions">
+          <button className="btn sign-up-btn">Sign Up</button>
+          <button className="btn sign-in-btn">Sign In</button>
+        </div>
+      </div>
     </main>
   );
 };
