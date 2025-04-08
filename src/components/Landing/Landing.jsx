82860b28100a9a1635e67ef2776d480bdeae62b7
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2025-04-08 16:19:20 -0700

    wip: added links to the signin and signout buttons

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './Landing.css';
 
 const Landing = () => {
@@ -7,8 +8,12 @@ const Landing = () => {
         <h1>Welcome to the Landing Page!</h1>
         <p>Sign up now, or sign in to see your super secret dashboard!</p>
         <div className="card-actions">
-          <button className="btn sign-up-btn">Sign Up</button>
-          <button className="btn sign-in-btn">Sign In</button>
+          <Link to="/sign-up">
+            <button className="btn sign-up-btn">Sign Up</button>
+          </Link>
+          <Link to="/sign-in">
+            <button className="btn sign-in-btn">Sign In</button>
+          </Link>
         </div>
       </div>
     </main>
